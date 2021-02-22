df34f797deeee1ddf0750a7cfc7d110da8067568
liarodina <rodinafla@yandex.ru>
2021-02-22 21:53:57 +0300

    создан тест test_add_contact выделены вспомогательные методы и классы

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+    def __init__(self, firstname, middlename, nickname, home, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.nickname = nickname
+        self.home = home
+        self.email = email
+
