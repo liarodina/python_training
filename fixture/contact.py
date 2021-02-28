4a8dfb258a94c4f8ba842677f300463e5ad591ed
liarodina <rodinafla@yandex.ru>
2021-02-28 22:40:43 +0300

    выделен класс ContactHelper и исправлены опечатки

---
+++ fixture/contact.py
@@ -0,0 +1,36 @@
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+
+    def create(self, contact):
+        # init contact creation
+        self.open_add_new_page()
+        wd = self.app.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit contact creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_home_page()
+
+    def open_add_new_page(self):
+        # open add new page
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
