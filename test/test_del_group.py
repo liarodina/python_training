994321acdb18b11d2575ccc7474a2871422255c0
liarodina <rodinafla@yandex.ru>
2021-03-05 18:23:24 +0300

    создан файл conftest.py

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,6 +3,6 @@ from model.group import Group
 
 def test_delete_first_group(app):
    app.session.login(username="admin", password="secret")
-   app.group.delete_first_group() (Group(name="qqq", header="qqq", footer="qqq"))
+   app.group.delete_first_group()
    app.session.logout()
 
