8fb862dd240aecfcaec2ce10114b9d73267e741a
liarodina <rodinafla@yandex.ru>
2021-03-06 18:05:47 +0300

    создан тест test_edit_first_group

--- fixture/group.py
+++ fixture/group.py
@@ -38,3 +38,18 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
+    def edit_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").send_keys("edit")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("edit")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("edit")
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
